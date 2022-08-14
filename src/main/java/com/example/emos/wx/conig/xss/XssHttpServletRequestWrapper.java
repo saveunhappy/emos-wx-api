e04844e935926436a7eff33654a1f85914ee7f6a
JiangTaoHou <925147954@qq.com>
2022-08-14 22:15:48 +0800

    抵御即跨站脚本

---
+++ src/main/java/com/example/emos/wx/conig/xss/XssHttpServletRequestWrapper.java
@@ -0,0 +1,72 @@
+package com.example.emos.wx.conig.xss;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HtmlUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+    public XssHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String getParameter(String name) {
+        String value = super.getParameter(name);
+        if (!StrUtil.hasEmpty(value)) {
+            value = HtmlUtil.filter(value);
+        }
+        return value;
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] values = super.getParameterValues(name);
+        if (values != null) {
+            for (int i = 0; i < values.length; i++) {
+                String value = values[i];
+                if (!StrUtil.hasEmpty(value)) {
+                    value = HtmlUtil.filter(value);
+                }
+                values[i] = value;
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+
+        Map<String, String[]> parameters = super.getParameterMap();
+        LinkedHashMap<String, String[]> map = new LinkedHashMap<>();
+        if (parameters != null) {
+            for (String key : parameters.keySet()) {
+                String[] values = parameters.get(key);
+                if (values != null) {
+                    for (int i = 0; i < values.length; i++) {
+                        String value = values[i];
+                        if (!StrUtil.hasEmpty(value)) {
+                            value = HtmlUtil.filter(value);
+                        }
+                        values[i] = value;
+                    }
+                    map.put(key, values);
+                }
+            }
+        }
+        return map;
+    }
+
+    @Override
+    public String getHeader(String name) {
+        String value = super.getHeader(name);
+        if (!StrUtil.hasEmpty(value)) {
+            value = HtmlUtil.filter(value);
+        }
+        return value;
+    }
+}
