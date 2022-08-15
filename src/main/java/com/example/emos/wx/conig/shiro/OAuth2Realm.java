53544d3880b619fbc984125f24926538f82a6ba3
JiangTaoHou <925147954@qq.com>
2022-08-15 22:18:19 +0800

    创建OAuth2Realm类

---
+++ src/main/java/com/example/emos/wx/conig/shiro/OAuth2Realm.java
@@ -0,0 +1,41 @@
+package com.example.emos.wx.conig.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+@Component
+public class OAuth2Realm extends AuthorizingRealm {
+    @Resource
+    private JwtUtil jwtUtil;
+
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof OAuth2Token;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        //TODO 查询用户的权限列表
+        //TODO 把权限列表添加到info对象中
+        return info;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        //TODO 从令牌中获取userId，然后检测该账户是否被冻结。
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo();
+        //TODO 往info对象中添加用户信息、Token字符串
+        return info;
+
+    }
+}
