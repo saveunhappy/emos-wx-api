e65d4422b49a54448b1226a366ad6790a13aa395
JiangTaoHou <925147954@qq.com>
2022-08-15 22:53:29 +0800

    创建ThreadLocalToken，作为传送token的媒介

---
+++ src/main/java/com/example/emos/wx/conig/shiro/ThreadLocalToken.java
@@ -0,0 +1,20 @@
+package com.example.emos.wx.conig.shiro;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ThreadLocalToken {
+    private final ThreadLocal<String> local = new ThreadLocal<>();
+
+    public void setToken(String token) {
+        local.set(token);
+    }
+
+    public String getToken() {
+        return local.get();
+    }
+
+    public void clear() {
+        local.remove();
+    }
+}
